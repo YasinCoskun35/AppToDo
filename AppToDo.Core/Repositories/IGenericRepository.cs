9aafd7d6007ba52dccef40052cf8d84fa7c9f52a
Yasin Coskun <yasincoskunn35@gmail.com>
2023-03-03 00:16:14 +0300

    Generic Repository Implementation

--- AppToDo.Core/Repositories/IGenericRepository.cs
+++ AppToDo.Core/Repositories/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace AppToDo.Core.Repositories
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(Guid id);
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
---
+++ AppToDo.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Linq.Expressions;
+using AppToDo.Core.Repositories;
+using AppToDo.Repository.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppToDo.Repository.Repositories
+{
+    public class GenericRepository<T>: IGenericRepository<T> where T : class
+    {
+        protected readonly AppToDoDbContext _context;
+        private  readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppToDoDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+           return _dbSet.Where(expression);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+           await _dbSet.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
